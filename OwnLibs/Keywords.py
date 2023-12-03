b6f197567e35a2c84223194e605b6a734a9abb7a
kamil <kamilgaldy@gmail.com>
2023-12-03 07:51:59 +0100

    correction to wait until keyword

--- OwnLibs/Keywords.py
+++ OwnLibs/Keywords.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 def lib_explicit_wait(driver, locator, key_wait_time=10):
     wait = WebDriverWait(driver, key_wait_time)
-    return wait.until(expected_conditions.visibility_of_element_located(*locator))
+    return wait.until(expected_conditions.visibility_of_element_located(locator))
 
 def lib_click(driver, locator, wait=True, key_wait_time=10):
     if wait:
